c073ac6bfdcda0db5be9f8e21da41209c347617e
elgroselj <ema.ljubljana@gmail.com>
2024-03-07 15:40:44 +0100

    Make plots, implement HS, fix LK

--- proj1/my.py
+++ proj1/my.py
@@ -6,18 +6,18 @@ sys.path.append(".")
 from proj1.ex1_utils import gaussderiv, show_flow
 
 
-def lucaskanade(im1,im2,N,SIG=0.5):
+def lucaskanade(im1,im2,N,SIG=0.5,TRESH=10**(-5)):
     #im1−first image matrix(grayscale)
     #im2−second image matrix(grayscale)
     #n−size of the neighborhood(NxN)
     
     Ix,Iy = gaussderiv(im1,SIG)
     It = im2-im1
-    Ixy = np.dot(Ix,Iy)
-    Ixx = np.dot(Ix,Ix)
-    Iyy = np.dot(Iy,Iy)
-    Ixt = np.dot(Ix,It)
-    Iyt = np.dot(Iy,It)
+    Ixy = np.multiply(Ix,Iy)
+    Ixx = np.multiply(Ix,Ix)
+    Iyy = np.multiply(Iy,Iy)
+    Ixt = np.multiply(Ix,It)
+    Iyt = np.multiply(Iy,It)
     
     kernel1 = np.ones((N,N), np.float32)
     suIxx = cv2.filter2D(src=Ixx, ddepth=-1, kernel=kernel1)
@@ -27,32 +27,49 @@ def lucaskanade(im1,im2,N,SIG=0.5):
     suIyt = cv2.filter2D(src=Iyt, ddepth=-1, kernel=kernel1)
     
 
-    D = np.dot(suIxx,suIyy) - np.dot(suIxy,suIxy)
-    U_denom = np.dot(suIyy,suIxt)-np.dot(suIxy,suIyt)
+    D = np.multiply(suIxx,suIyy) - np.multiply(suIxy,suIxy)
+    mask = np.abs(D) < TRESH
+    D[mask & (D > 0)] = TRESH
+    D[mask & (D == 0)] = TRESH
+    D[mask & (D < 0)] = -TRESH
+    
+    
+    U_denom = np.multiply(suIyy,suIxt)-np.multiply(suIxy,suIyt)
     U = -np.divide(U_denom,D)
-    V_denom = np.dot(suIxx,suIyt)-np.dot(suIxy,suIxt)
+    V_denom = np.multiply(suIxx,suIyt)-np.multiply(suIxy,suIxt)
     V = -np.divide(V_denom,D)
+    
+    
     
     return U,V
     
-    
-    
-    # u = 
-    # plt.imshow(suIxx)
-    # plt.show()
-    
-    
-# im1 = np.ones((9,9), np.float32)/2
-# im2 = np.ones((9,9), np.float32)/3
 
-# im1 = np.eye(9,9)+np.eye(9,9,k=1)
-# im2 = np.eye(9,9,k=1)+np.eye(9,9,k=2)
-# print(im1,im2)
-
-
-# U,V = lucaskanade(im1,im2,3)
-
-# show_flow(U,V,plt.gca())
-# plt.show()
-
+def hornschunck(im1,im2,n_iters,lmbd,SIG = 0.9):
+    #im1−first imagematrix(grayscale)
+    #im2−second imagematrix(grayscale)
+    #n_iters−number of iterations(tryseveralhundred)
+    #lmbd−parameter
     
+    u = np.zeros(im1.shape)
+    v = np.zeros(im1.shape)
+    Ld = np.array([[0,1,0],[1,0,1],[0,1,0]]) * 0.25
+    
+    Ix,Iy = gaussderiv(im1,SIG)
+    Ix2 = np.multiply(Ix,Ix)
+    Iy2 = np.multiply(Iy,Iy)
+    D = lmbd + Ix2 + Iy2
+    It = im2-im1
+    
+    for _ in range(n_iters):
+        ua = cv2.filter2D(src=u, ddepth=-1, kernel=Ld)
+        va = cv2.filter2D(src=v, ddepth=-1, kernel=Ld)
+        
+        P = np.multiply(Ix,ua) + np.multiply(Iy,va) + It
+        
+        u = ua - np.multiply(Ix,np.divide(P,D))
+        v = va - np.multiply(Iy,np.divide(P,D))
+        
+    return u,v
+        
+    
+
--- proj1/snnipet_old.py
+++ proj1/snnipet_old.py
@@ -47,23 +47,25 @@ tresh = 10**(-5)
 
 
 U_lk, V_lk = lucaskanade(im1, im2, 3, SIG=sig, TRESH=tresh)
-U_hs , V_hs = hornschunck( im1 , im2 , 1000 , 0.5 )
 
 
-
-fig1, ((ax1_11, ax1_12), (ax1_21, ax1_22), (ax1_31, ax1_32)) = plt.subplots(3, 2)
-for tup in ((ax1_11, ax1_12), (ax1_21, ax1_22), (ax1_31, ax1_32)):
-    for ax in tup:
-        ax.set_axis_off()
-# fig1.tight_layout()
-fig1.subplots_adjust(wspace=0, hspace=0)
+fig1, ((ax1_11, ax1_12), (ax1_21, ax1_22)) = plt.subplots(2, 2)
 ax1_11.imshow(im1)
 ax1_12.imshow(im2)
 show_flow(U_lk, V_lk, ax1_21, type='angle')
 show_flow(U_lk, V_lk, ax1_22, type='field', set_aspect=True)
-show_flow(U_hs , V_hs , ax1_31 , type='angle')
-show_flow(U_hs, V_hs, ax1_32, type='field',set_aspect=True)
+fig1.suptitle('Lucas−Kanade Optical Flow')
 
-# fig1.suptitle('Lucas−Kanade and Horn−Schunck Optical Flow')
+plt.savefig(im_name+"_lk.svg")
 
-plt.savefig(im_name+".svg",bbox_inches="tight",pad_inches=0)
+
+# U_hs , V_hs = hornschunck( im1 , im2 , 1000 , 0.5 )
+
+# fig2, ((ax2_11, ax2_12), (ax2_21, ax2_22)) = plt.subplots(2, 2)
+
+# ax2_11.imshow( im1 )
+# ax2_12.imshow( im2 )
+# show_flow(U_hs , V_hs , ax2_21 , type='angle')
+# show_flow(U_hs, V_hs, ax2_22, type='field',set_aspect=True)
+# fig2.suptitle('Horn−Schunck OpticalFlow')
+# plt.savefig(im_name+"_hs.svg")
