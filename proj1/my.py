789d6fe3fc01a40feb7b7c9066615c1a0aa8909f
elgroselj <ema.ljubljana@gmail.com>
2024-03-11 16:10:53 +0100

    Harris, timing, params eval...

--- proj1/my.py
+++ proj1/my.py
@@ -3,10 +3,10 @@ from matplotlib import pyplot as plt
 import numpy as np
 import sys
 sys.path.append(".")
-from proj1.ex1_utils import gaussderiv, show_flow
+from proj1.ex1_utils import gaussderiv, show_flow, gausssmooth
 
 
-def lucaskanade(im1,im2,N,SIG=0.5,TRESH=10**(-5)):
+def lucaskanade(im1,im2,N,SIG=0.9,TRESH=10**(-5),R=None,R_tresh=0.6):
     #im1−first image matrix(grayscale)
     #im2−second image matrix(grayscale)
     #n−size of the neighborhood(NxN)
@@ -19,6 +19,7 @@ def lucaskanade(im1,im2,N,SIG=0.5,TRESH=10**(-5)):
     Ixt = np.multiply(Ix,It)
     Iyt = np.multiply(Iy,It)
     
+    assert(N%2==1)    
     kernel1 = np.ones((N,N), np.float32)
     suIxx = cv2.filter2D(src=Ixx, ddepth=-1, kernel=kernel1)
     suIxy = cv2.filter2D(src=Ixy, ddepth=-1, kernel=kernel1)
@@ -39,19 +40,27 @@ def lucaskanade(im1,im2,N,SIG=0.5,TRESH=10**(-5)):
     V_denom = np.multiply(suIxx,suIyt)-np.multiply(suIxy,suIxt)
     V = -np.divide(V_denom,D)
     
+    if R is not None:
+        U[R<R_tresh]=0
+        V[R<R_tresh]=0
+    
+    # if R is not None:
+    #     U[R==0]=0
+    #     V[R==0]=0
+    
     
     
     return U,V
     
 
-def hornschunck(im1,im2,n_iters,lmbd,SIG = 0.9):
+def hornschunck(im1,im2,n_iters,lmbd,SIG = 0.9,u0=None,v0=None):
     #im1−first imagematrix(grayscale)
     #im2−second imagematrix(grayscale)
     #n_iters−number of iterations(tryseveralhundred)
     #lmbd−parameter
     
-    u = np.zeros(im1.shape)
-    v = np.zeros(im1.shape)
+    u = np.zeros(im1.shape) if u0 is None else u0
+    v = np.zeros(im1.shape) if v0 is None else v0
     Ld = np.array([[0,1,0],[1,0,1],[0,1,0]]) * 0.25
     
     Ix,Iy = gaussderiv(im1,SIG)
@@ -70,6 +79,52 @@ def hornschunck(im1,im2,n_iters,lmbd,SIG = 0.9):
         v = va - np.multiply(Iy,np.divide(P,D))
         
     return u,v
+    
+    
+def harris_response(im,SIG=1,kernel_size=6,alpha=0.06,tresh=0.95):
+    Ix,Iy = gaussderiv(im,sigma=SIG)
+    a = gausssmooth(Ix*Ix,sigma=SIG)
+    b = gausssmooth(Iy*Iy,sigma=SIG)
+
+    det = np.multiply(a,b)
+    trace = a+b
+
+    R = det - alpha * np.multiply(trace,trace)
+    R = R-np.amin(R)
+    R = R/np.amax(R)
+          
+
+    def points_nonmax_tresh(hp,tresh=tresh,box_size=2):
+        def nonmaxima_suppression_box(A,box_size=2):
+            
+            def loc_max_box(A,y,x,box_size=1):#9,25
+                if box_size == 1:
+                    return (A[y,x] >= max(A[y+1,x-1],A[y+1,x+1],A[y+1,x],A[y,x+1]) and A[y,x] > max(A[y,x-1],A[y-1,x-1],A[y-1,x],A[y-1,x+1]))
+                elif box_size == 2:
+                    return (A[y,x] > max(A[y-2,x-2], A[y-2,x-1], A[y-2,x], A[y-2,x+1], A[y-2,x+2],
+                                        A[y-1,x-2], A[y-1,x-1], A[y-1,x], A[y-1,x+1], A[y-1,x+2],
+                                        A[y,x-2],   A[y,x-1]) and
+                            A[y,x] >= max(A[y,x+1], A[y,x+2],
+                                        A[y+1,x-2], A[y+1,x-1], A[y+1,x], A[y+1,x+1], A[y+1,x+2],
+                                        A[y+2,x-2], A[y+2,x-1], A[y+2,x], A[y+2,x+1], A[y+2,x+2]))
+                    
+            A_ = np.copy(A)
+            for x in range(box_size,len(A[0])-box_size):
+                for y in range(box_size,len(A)-box_size):
+                    if not loc_max_box(A_,y,x,box_size):
+                        A_[y,x] = 0
+            return(A_)
+        
+        hp_ = nonmaxima_suppression_box(hp,box_size)
+        hp_ = np.where(hp_>tresh,1,0)
+        return(hp_)
+    
+    
+    # R_ = points_nonmax_tresh(R,tresh=tresh)
+    # return(R_)
+    return R
+    
+    
         
     
 
