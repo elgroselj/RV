becc69c7f5b82fba51a94a3ebaaed936b4061d10
elgroselj <ema.ljubljana@gmail.com>
2024-03-31 14:24:28 +0200

    uporaba njihovih funkcij, komentarji v ms tracker

--- proj2/ms_material/ms_tracker.py
+++ proj2/ms_material/ms_tracker.py
@@ -4,17 +4,23 @@ import cv2
 import sys
 sys.path.append(".")
 from ex2_utils import Tracker
+from ms_material.ex2_utils import create_epanechnik_kernel, \
+            get_patch, extract_histogram, backproject_histogram
 
 
-class NCCTracker(Tracker):
+
+class MeanShiftTracker(Tracker):
 
     def initialize(self, image, region):
+        # region tuple dolzine 4 (oz. 8)  
+        #  0: col , 1: row , 2: delta col, 3: delta row    
 
         if len(region) == 8:
             x_ = np.array(region[::2])
             y_ = np.array(region[1::2])
             region = [np.min(x_), np.min(y_), np.max(x_) - np.min(x_) + 1, np.max(y_) - np.min(y_) + 1]
 
+        # window?? = veckratnik dolzine vecje od stranic
         self.window = max(region[2], region[3]) * self.parameters.enlarge_factor
 
         left = max(region[0], 0)
@@ -24,30 +30,49 @@ class NCCTracker(Tracker):
         bottom = min(region[1] + region[3], image.shape[0] - 1)
 
         self.template = image[int(top):int(bottom), int(left):int(right)]
+        # lahko NEcelo stevilo
+        # sredina regiona
         self.position = (region[0] + region[2] / 2, region[1] + region[3] / 2)
         self.size = (region[2], region[3])
+        
+        # zracunam histogram q z Epanechnikovim jedrom
+        width = abs(top-bottom)
+        height = abs(right-left)
+        kernel = create_epanechnik_kernel(width, height, sigma=1)
+        # self.template = self.template*255 if max(image) <= 1 else self.template
+        hist = extract_histogram(self.template, nbins=16, weights=kernel)
+        self.q = hist/np.sum(hist)
 
     def track(self, image):
 
+        # krajisca preiskovane okolice (vecji bounding box)
         left = max(round(self.position[0] - float(self.window) / 2), 0)
         top = max(round(self.position[1] - float(self.window) / 2), 0)
 
         right = min(round(self.position[0] + float(self.window) / 2), image.shape[1] - 1)
         bottom = min(round(self.position[1] + float(self.window) / 2), image.shape[0] - 1)
 
+        # ce je template manjsi od okolice , se prestavim za pol velikosti desno, dol
+        # ?? al je to sam rezlika v def pos in bbox
         if right - left < self.template.shape[1] or bottom - top < self.template.shape[0]:
             return [self.position[0] + self.size[0] / 2, self.position[1] + self.size[1] / 2, self.size[0], self.size[1]]
 
+        # okolico izrezem iz slike
         cut = image[int(top):int(bottom), int(left):int(right)]
 
         matches = cv2.matchTemplate(cut, self.template, cv2.TM_CCOEFF_NORMED)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(matches)
+        _, _, min_loc, max_loc = cv2.minMaxLoc(matches)
 
+        # ????????????????????????????????????????
+        # left + max_loc[0] + float(self.size[0]) / 2
+        # sredina_regiona <- (krajicse_okolice + max) + pol vel regiona
+        # + pol vel regiona, pretvorba iz krajisca templata (kjer najboljse ujemanje) v sredisce
         self.position = (left + max_loc[0] + float(self.size[0]) / 2, top + max_loc[1] + float(self.size[1]) / 2)
 
+        # koordinati krajisca zgoraj levo, velikosti regiona
         return [left + max_loc[0], top + max_loc[1], self.size[0], self.size[1]]
 
-class NCCParams():
+class MSParams():
     def __init__(self):
         self.enlarge_factor = 2
 
--- proj2/my_functions.py
+++ proj2/my_functions.py
@@ -5,7 +5,7 @@ import cv2 as cv
 
 import sys
 sys.path.append(".")
-from ms_material.ex2_utils import generate_responses_1, create_epanechnik_kernel, get_patch
+from ms_material.ex2_utils import generate_responses_1, create_epanechnik_kernel, get_patch, extract_histogram, backproject_histogram
 
 
 
@@ -25,52 +25,68 @@ from ms_material.ex2_utils import generate_responses_1, create_epanechnik_kernel
 # k = lambda y: 1-y
 # g = lambda y: 1
 
-def mean_shift_epanechnik(pdf, x0, n_iter, h, hs= None, plot=False):
+
+#REGION = obmocje, kjer iscem
+#TARGET = je pa smiley al karkol kar smo vedeli, kje je v 1. framu, in nas zanima, kje je v 2. framu
+
+
+def mean_shift_epanechnik(pdf, x0, n_iter, width, height, plot=False):
     # pdf           pdf matrix
     # x0 = (a,b)    start position
     # n_iter        number of iterations
-    # h region size
+    # width, height region size
     print("x0: ", x0)
     x = x0
     xs = [tuple(x)]
-    if hs is None:
-        hs = [h]
+    
+    
         
     if plot:
         plt.imshow(pdf)
+
+    assert width % 2 == 1
+    assert height % 2 == 1
     
-    COLORS = "rgb"
-    for hi, h in enumerate(hs):
-        assert h % 2 == 1
-        pol = int(h/2)
-        gradnik = np.arange(-pol,pol+1).reshape(h,1)
-        indexy = np.repeat(gradnik, h, axis=1)
-        indexx = indexy.transpose()
-        for i in range(n_iter):
-            patch, _ = get_patch(pdf, x, (h,h))
-            # print(patch)
-            # plt.imshow(patch)
-            # plt.show()
-            xx = np.sum(np.multiply(patch, indexx))
-            yy = np.sum(np.multiply(patch, indexy))
-            norm = np.sum(patch)
-            # print(xx,yy,norm)
-            x_ = np.array([xx, yy]) / norm
-            # print(x_)
-            x_ = [int(round(x_[0])),int(round(x_[1]))]
-            # print(x_)
-            x[0] += x_[0]
-            x[1] += x_[1]
-            xs.append(tuple(x))
-            if x_ == [0,0]:
-                print("Break: close enough n_iter: ", i)
-                break
-            print(x)
+    
+    #sestavim indeksni matriki
+    pol_height = int(height/2)
+    gradnik = np.arange(-pol_height,pol_height+1).reshape(height,1)
+    indexy = np.repeat(gradnik, width, axis=1)
+    
+    pol_width = int(width/2)
+    gradnik = np.arange(-pol_width,pol_width+1).reshape(width,1)
+    indexx = np.repeat(gradnik, height, axis=1)
+    
+    indexx = indexx.transpose()
+    
+    
+    for i in range(n_iter):
+        patch, _ = get_patch(pdf, x, (width,height))
+        # print(patch)
+        # plt.imshow(patch)
+        # plt.show()
+        xx = np.sum(np.multiply(patch, indexx))
+        yy = np.sum(np.multiply(patch, indexy))
+        norm = np.sum(patch)
+        # print(xx,yy,norm)
+        x_ = np.array([xx, yy]) / norm
+        # print(x_)
+        x_ = [int(round(x_[0])),int(round(x_[1]))]
+        # print(x_)
+        if x_ == [0,0]:
+            print("Break: close enough n_iter: ", i)
+            break
         
-        if plot:
-            a = np.array(xs)
-            plt.scatter(a[:,0],a[:,1],s=5,c=COLORS[hi%len(COLORS)],marker="o")
-            xs = []
+        x[0] += x_[0]
+        x[1] += x_[1]
+        xs.append(tuple(x))
+        
+        print(x)
+    
+    if plot:
+        a = np.array(xs)
+        plt.scatter(a[:,0],a[:,1],s=5,c='r',marker="o")
+        xs = []
     plt.show()      
     return x
         
@@ -78,7 +94,8 @@ def mean_shift_epanechnik(pdf, x0, n_iter, h, hs= None, plot=False):
 
 # im = generate_responses_1()
 
-# mean_shift_epanechnik(im, [20,20], n_iter = 10, h =9,plot=True)
+# mean_shift_epanechnik(im, [20,20], n_iter = 10, width=9, height=5,plot=False)
+# mean_shift_epanechnik(im, [60,60], n_iter = 10, width=21, height=19, plot=True)
 
 
 
@@ -101,71 +118,71 @@ def mean_shift_epanechnik(pdf, x0, n_iter, h, hs= None, plot=False):
 # mean_shift_epanechnik(im, [70,40], n_iter = 10, h =51,plot=True)
 # mean_shift_epanechnik(im, [70,40], n_iter = 10, h = "kr neki", hs = [51,21,11,3], plot=True)
 
-def weighted_color_histogram_epanechnik(region,m,sigma=1, plot=False):
-    region = np.array(region)
-    for ch in range(3):
-        region[:,:,ch] = np.floor(region[:,:,ch] / 255 * (m-1)).astype(np.int32)
+# def weighted_color_histogram_epanechnik(region,m,sigma=1, plot=False):
+#     region = np.array(region)
+#     for ch in range(3):
+#         region[:,:,ch] = np.floor(region[:,:,ch] / 255 * (m-1)).astype(np.int32)
         
         
-    w, h = region.shape
-    kernel = create_epanechnik_kernel(w, h, sigma)
-    kernel = kernel[:h,:w].transpose()
+#     w, h = region.shape
+#     kernel = create_epanechnik_kernel(w, h, sigma)
+#     kernel = kernel[:h,:w].transpose()
     
     
-    hist = np.zeros((16,16,16))
-    for i in range(region.shape[0]):
-        for j in range(region.shape[1]):
-            r,g,b = region[i,j,:]
-            hist[r,g,b]+=kernel[i,j]
+#     hist = np.zeros((16,16,16))
+#     for i in range(region.shape[0]):
+#         for j in range(region.shape[1]):
+#             r,g,b = region[i,j,:]
+#             hist[r,g,b]+=kernel[i,j]
         
         
 
 
 
-def binarize(region, m):
-    return np.floor(region / 255 * (m-1)).astype(np.int32)
+# def binarize(region, m):
+#     return np.floor(region / 255 * (m-1)).astype(np.int32)
     
 
-def weighted_color_histogram_epanechnik(region,m,sigma=1, plot=False):
-    # region
-    # m         number of distinct colors (bins)
-    # print(region)
-    region_binarized = binarize(region, m)
-    # print(region_binarized)
-    # plt.imshow(region_binarized)
-    # plt.show()
+# def weighted_color_histogram_epanechnik(region,m,sigma=1, plot=False):
+#     # region
+#     # m         number of distinct colors (bins)
+#     # print(region)
+#     region_binarized = binarize(region, m)
+#     # print(region_binarized)
+#     # plt.imshow(region_binarized)
+#     # plt.show()
     
     
-    w, h = region.shape
-    kernel = create_epanechnik_kernel(w, h, sigma)
-    kernel = kernel[:h,:w].transpose()
+#     w, h = region.shape
+#     kernel = create_epanechnik_kernel(w, h, sigma)
+#     kernel = kernel[:h,:w].transpose()
     
-    region_binarized_flat = region_binarized.reshape(-1)
-    kernel_flat = kernel.reshape(-1)
+#     region_binarized_flat = region_binarized.reshape(-1)
+#     kernel_flat = kernel.reshape(-1)
     
-    hist = [0]*m
-    for c, v in zip(region_binarized_flat,kernel_flat):
-        hist[c] += v
+#     hist = [0]*m
+#     for c, v in zip(region_binarized_flat,kernel_flat):
+#         hist[c] += v
         
-    s = sum(hist)
-    hist = [x/s for x in hist]
+#     s = sum(hist)
+#     hist = [x/s for x in hist]
         
-    if plot:
-        plt.bar(range(m),hist)
-        plt.show()
+#     if plot:
+#         plt.bar(range(m),hist)
+#         plt.show()
         
-    return hist
+#     return hist
 
-def weighted_color_histogram_epanechnik_RGB(region,m,sigma=1, plot=False):
-    hists = []
-    for ch in range(region.shape[2]):
-        hist = weighted_color_histogram_epanechnik(region[:,:,ch],m,sigma=1, plot=plot)
-        hists.append(hist)
-    hists = np.array(hists)
-    s = np.sum(hists)
-    hists /= s
+# def weighted_color_histogram_epanechnik_RGB(region,m,sigma=1, plot=False):
+#     hists = []
+#     for ch in range(region.shape[2]):
+#         hist = weighted_color_histogram_epanechnik(region[:,:,ch],m,sigma=1, plot=plot)
+#         hists.append(hist)
+#     hists = np.array(hists)
+#     s = np.sum(hists)
+#     hists /= s
     
-    return hists
+#     return hists
         
 
 # im = np.array([[1,0,1],[0,2,0],[1,0,1]]) * 255/2
@@ -187,36 +204,116 @@ def weighted_color_histogram_epanechnik_RGB(region,m,sigma=1, plot=False):
 
 # target je q (hist_old), candidati so p
 
-            
-def main(imgs,m,x0,h,n_iter,plot =False, plot_hists=False, plot_mean_shift=False):
-    x = x0
-    xs = [tuple(x)]
-    for i in range(len(imgs)-1):
-        im1 = imgs[i]
-        qs = weighted_color_histogram_epanechnik_RGB(im1,m=m,plot=plot_hists)
-        im2 = imgs[i+1]
-        ps = weighted_color_histogram_epanechnik_RGB(im2,m=m,plot=plot_hists)
-        W = np.zeros(im1.shape[:2])
-        for ch in range(3):
-            B = binarize(im2[:,:,ch],m) # b(x_i)
-            for r in range(B.shape[0]):
-                for c in range(B.shape[1]):
-                    W[r,c] += np.sqrt(qs[ch][int(B[r,c])] / ps[ch][int(B[r,c])])
-        x = mean_shift_epanechnik(W, x, n_iter=n_iter, h=h, plot=plot_mean_shift)
-        if plot:
-            plt.imshow(im2)
-            plt.scatter(x[0],x[1])
-        xs.append(tuple(x))
-    return xs
-
-
+width, height = 51,51
 im1 = plt.imread("/home/lema/Documents/RV/proj1/spegli1.jpg")
-im1 = cv.resize(im1, (0, 0), fx = 0.1, fy = 0.1)
-# qs = weighted_color_histogram_epanechnik_RGB(im,m=16,plot=True)
 im2 = plt.imread("/home/lema/Documents/RV/proj1/spegli2.jpg")
-im2 = cv.resize(im2, (0, 0), fx = 0.1, fy = 0.1)
-# ps = weighted_color_histogram_epanechnik_RGB(im,m=16,plot=True)
-main([im1,im2],m=16,x0=[75,75],h=201,n_iter=200,plot=True,plot_hists=False,plot_mean_shift=True)
+# im1 = plt.imread("/home/lema/Documents/RV/proj2/barve.png")
+x0 = [700,700]
+plt.imshow(im1)
+plt.show()
+
+kernel = create_epanechnik_kernel(width, height, sigma=1)
+nbins=16
+
+patch1,_ = get_patch(im1, x0, (width,height))
+plt.imshow(patch1)
+plt.show()
+hist = extract_histogram(patch1*255, nbins=nbins, weights=kernel)
+q = hist/np.sum(hist)
+
+patch2,_ = get_patch(im2, x0, (width,height))
+plt.imshow(patch2)
+plt.show()
+hist = extract_histogram(patch2*255, nbins=nbins, weights=kernel)
+p = hist/np.sum(hist)
+
+#predracunamo
+epsi = 1e-3 #TODO pazi
+# V = {}
+# for u in range(len(p)):
+#     V[u]=np.sqrt(q[u]/(p[u]+epsi))
+# V = np.sqrt(q/(p+epsi))
+
+region,_ = get_patch(im2, x0, (width*2+1,height*2+1))
+plt.imshow(region)
+plt.show()
+#TODO tuki ne vem ker hist uporabit od q al od p (tj b(xi) je bin barve od xi v im1 al im2)
+backprojection_q = backproject_histogram(region, q, nbins=nbins)
+backprojection_p = backproject_histogram(region, p, nbins=nbins)
+plt.imshow(backprojection_q)
+plt.colorbar()
+plt.show()
+plt.imshow(backprojection_p)
+plt.colorbar()
+plt.show()
+
+
+W = np.zeros((region.shape[0],region.shape[1]))
+for i in range(region.shape[0]):
+    for j in range(region.shape[1]):
+        W[i,j] = np.sqrt(backprojection_q[i,j] / (backprojection_p[i,j] + epsi))
+        
+plt.imshow(W)
+plt.colorbar()
+plt.show()
+
+x1 = mean_shift_epanechnik(W, x0=[0,0], n_iter=100, width=21, height=21, plot=True)
+patch3,_ = get_patch(im2, x1, (width,height))
+plt.imshow(patch3)
+plt.show()
+
+
+
+    
+
+    
+    
+
+
+# plt.bar(list(range(len(hist))),hist,width=10)
+# print((range(len(hist)),hist))
+# # plt.hist(hist)
+# plt.show()
+
+
+
+
+
+
+
+
+
+
+            
+# def main(imgs,m,x0,h,n_iter,plot =False, plot_hists=False, plot_mean_shift=False):
+#     x = x0
+#     xs = [tuple(x)]
+#     for i in range(len(imgs)-1):
+#         im1 = imgs[i]
+#         qs = weighted_color_histogram_epanechnik_RGB(im1,m=m,plot=plot_hists)
+#         im2 = imgs[i+1]
+#         ps = weighted_color_histogram_epanechnik_RGB(im2,m=m,plot=plot_hists)
+#         W = np.zeros(im1.shape[:2])
+#         for ch in range(3):
+#             B = binarize(im2[:,:,ch],m) # b(x_i)
+#             for r in range(B.shape[0]):
+#                 for c in range(B.shape[1]):
+#                     W[r,c] += np.sqrt(qs[ch][int(B[r,c])] / ps[ch][int(B[r,c])])
+#         x = mean_shift_epanechnik(W, x, n_iter=n_iter, h=h, plot=plot_mean_shift)
+#         if plot:
+#             plt.imshow(im2)
+#             plt.scatter(x[0],x[1])
+#         xs.append(tuple(x))
+#     return xs
+
+
+# im1 = plt.imread("/home/lema/Documents/RV/proj1/spegli1.jpg")
+# im1 = cv.resize(im1, (0, 0), fx = 0.1, fy = 0.1)
+# # qs = weighted_color_histogram_epanechnik_RGB(im,m=16,plot=True)
+# im2 = plt.imread("/home/lema/Documents/RV/proj1/spegli2.jpg")
+# im2 = cv.resize(im2, (0, 0), fx = 0.1, fy = 0.1)
+# # ps = weighted_color_histogram_epanechnik_RGB(im,m=16,plot=True)
+# main([im1,im2],m=16,x0=[75,75],h=201,n_iter=200,plot=True,plot_hists=False,plot_mean_shift=True)
     
         
     
