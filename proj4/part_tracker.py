165c622eb4f99fbf5ad68eea70f869a151205f97
elgroselj <ema.ljubljana@gmail.com>
2024-05-09 09:31:44 +0200

    Nove slikce

--- proj4/part_tracker.py
+++ proj4/part_tracker.py
@@ -7,13 +7,16 @@ from ex4_utils import sample_gauss
 
 
 class PartTracker(Tracker):
-    def visualize(bag, image):
+    def visualize(bag, image, left, right, bottom, top):
         plt.imshow(image)
         points = np.array([pair["state"][:2] for pair in bag])
         if points.shape[0] > 1:
             plt.scatter(points[:,0],points[:,1],c="r",s=10)
         else:
             plt.scatter(points[0],points[1],c="r",s=10)
+            
+        plt.scatter(left,bottom,c="pink")
+        plt.scatter(right,top,c="purple")
         plt.show()
     def visualize1(location, image):
         plt.imshow(image)
@@ -38,7 +41,7 @@ class PartTracker(Tracker):
         
         # zracunam histogram q z Epanechnikovim jedrom
         height, width = self.template.shape[:2]
-        self.epanechnik_kernel = create_epanechnik_kernel(width, height, sigma=1)
+        self.epanechnik_kernel = create_epanechnik_kernel(width, height, sigma=self.parameters.sigma_epanachnich)
         # self.template = self.template*255 if max(image) <= 1 else self.template
         hist = extract_histogram(self.template,
                                  nbins=self.parameters.nbins,
@@ -48,9 +51,50 @@ class PartTracker(Tracker):
         # noise = sample_gauss(self.parameters.mu, self.parameters.sigma, self.parameters.n)
         # self.bag = [(self.position[0] + x, self.position[1] + y, 1) for x,y in noise]
         samples = sample_gauss(self.position, np.eye(2) * self.parameters.sigma, self.parameters.n)
-        self.bag = [{"state":np.array([x,y,0,0]), "weight": 1} for x,y in samples]
+
         if self.parameters.verbose:
-            PartTracker.visualize(self.bag, image)
+            PartTracker.visualize(self.bag, image,left, right, bottom, top)
+            
+        if self.parameters.dynamic_mode == "RW":
+            # RW
+            self.bag = [{"state":np.array([x,y]), "weight": 1} for x,y in samples]
+            
+            self.A = np.array([[1,0],
+                                [0,1]])
+            self.Q = np.array([[1,0],
+                                [0,1]])
+        elif self.parameters.dynamic_mode == "NCV":
+            # NCV
+            self.bag = [{"state":np.array([x,y,0,0]), "weight": 1} for x,y in samples]
+            
+            self.A = np.array([[1,0,1,0],
+                                [0,1,0,1],
+                                [0,0,1,0],
+                                [0,0,0,1]])
+            self.Q = 1/6 * np.array([[2,0,3,0],
+                                    [0,2,0,3],
+                                    [3,0,6,0],
+                                    [0,3,0,6]])
+        if self.parameters.dynamic_mode == "NCA":
+            # NCA
+            self.bag = [{"state":np.array([x,y,0,0,0,0]), "weight": 1} for x,y in samples]
+            
+            self.A = 1/2 * np.array([[2,0,2,0,1,0],
+                                    [0,2,0,2,0,1],
+                                    [0,0,2,0,2,0],
+                                    [0,0,0,2,0,2],
+                                    [0,0,0,0,2,0],
+                                    [0,0,0,0,0,2]])
+            
+            self.Q = np.array([[1/20,0,1/8,0,1/6,0],
+                                    [0,1/20,0,1/8,0,1/6],
+                                    [1/8,0,1/3,0,1/2,0],
+                                    [0,1/8,0,1/3,0,1/2],
+                                    [1/6,0,1/2,0,1,0],
+                                    [0,1/6,0,1/2,0,1]])
+        
+        
+        self.Q = (height+width)/2 * self.parameters.q * self.Q
 
     def track(self, image):
 
@@ -66,33 +110,64 @@ class PartTracker(Tracker):
         # cut = image[int(top):int(bottom), int(left):int(right)]
         
         ###########################################################
+        
+                    
         # resample
         p = [pair["weight"] for pair in self.bag]
         p = np.array(p)/sum(p)
         self.bag = choice(self.bag, self.parameters.n, p=p, replace=True)
         
         if self.parameters.verbose:
-            PartTracker.visualize(self.bag, image)
+            PartTracker.visualize(self.bag, image,left, right, bottom, top)
         
         # dynamic model
         
         for pair in self.bag:
-            pair["state"] = np.dot(self.parameters.A, np.array(pair["state"]))
-            pair["state"] += sample_gauss([0,0,0,0], self.parameters.Q, 1).flatten()
-            
-        # ce gre ven iz search region
-        # 1 ga zavrzem
-        # self.bag = [pair for pair in self.bag if pair["state"][0] >= left and pair["state"][0] <= right 
-        #                                         and pair["state"][1] >= top and pair["state"][1] <= bottom]
-        
-        # 2 mu dam utež na 0
-        for pair in self.bag:
-            if pair["state"][0] >= left and pair["state"][0] <= right \
-                and pair["state"][1] >= top and pair["state"][1] <= bottom:
-                    pair["weight"] = 0
+            pair["state"] = np.dot(self.A, np.array(pair["state"]))
+            pair["state"] += sample_gauss([0,0,0,0], self.Q, 1).flatten()
             
         if self.parameters.verbose:
-            PartTracker.visualize(self.bag, image)
+            PartTracker.visualize(self.bag, image,left, right, bottom, top)
+            
+        # # 1 ga zavrzem
+        # self.bag = [pair for pair in self.bag if pair["state"][0] > left and pair["state"][0] < right 
+        #             and pair["state"][1] > top and pair["state"][1] < bottom]
+        
+        # ce gre ven iz search region
+                
+        # mu dam utež na 0
+        for pair in self.bag:
+            # if pair["state"][1] >= left and pair["state"][1] <= right \
+            #     or pair["state"][0] >= top and pair["state"][0] <= bottom:
+            #         pair["weight"] = 0
+                    
+            #         print(pair["state"][0])
+            #         pair["state"][0] = np.clip(pair["state"][0],top,bottom)
+            #         print(pair["state"][0])
+                    
+            #         print(pair["state"][1])
+            #         pair["state"][1] = np.clip(pair["state"][1],left,right)
+            #         print(pair["state"][1])
+            if pair["state"][0] < left or pair["state"][0] > right:
+                pair["weight"] = 0
+                # print()
+                # print(pair["state"][0])
+                pair["state"][0] = np.clip(pair["state"][0],left,right)
+                # print(pair["state"][0])
+                
+                
+            if pair["state"][1] < top or pair["state"][1] > bottom:
+                pair["weight"] = 0
+        
+                # print()
+                # print(pair["state"][1])
+                pair["state"][1] = np.clip(pair["state"][1],top,bottom)
+                # print(pair["state"][1])
+            
+        
+            
+        if self.parameters.verbose:
+            PartTracker.visualize(self.bag, image,left, right, bottom, top)
             
         def hell_dist(p,q):
             P = p.flatten()
@@ -141,37 +216,39 @@ class PartTracker(Tracker):
         self.q = (1 - self.parameters.alpha) * self.q + self.parameters.alpha * p
         
         #############################################33
-
-        # assert (self.position [0] == left + max_loc[0] + float(self.size[0]) / 2 and
-        #         self.position[1] == top + max_loc[1] + float(self.size[1]) / 2)
+        # print(self.position[0], left + max_loc[0] + float(self.size[0]) / 2)
+        # print(self.position[1], top + max_loc[1] + float(self.size[1]) / 2)        
+        assert (abs(self.position[0] - (left + max_loc[0] + float(self.size[0]) / 2)) < 1e-7 and
+                abs(self.position[1] - (top + max_loc[1] + float(self.size[1]) / 2)) < 1e-7)
+        
 
         return [left + max_loc[0], top + max_loc[1], self.size[0], self.size[1]]
 
 class PartParams():
     def __init__(self):
-        # self.enlarge_factor = 2
-        self.enlarge_factor = 2.5
+        self.enlarge_factor = 1
         
-        self.nbins = 16
+        self.sigma_epanachnich = 2
+        self.nbins = 8
         self.hist_mode = "BGR"
-        self.alpha = 0.1 # spreminjanje tergeta
+        self.alpha = 0.01 # spreminjanje tergeta
+        # self.alpha = 0.03 # spreminjanje tergeta
         
-        self.q = 1 # dependent on target size # kolko naprej skočijo pikice v dinamičnem modelu
-        self.r = 1 # koliko je šuma v podatkih
+        # self.q = 1 # dependent on target size # kolko naprej skočijo pikice v dinamičnem modelu
+        self.q = 0.25 # dependent on target size # kolko naprej skočijo pikice v dinamičnem modelu
+        # self.r = 1 # koliko je šuma v podatkih
+        self.r = 2 # koliko je šuma v podatkih
         
-        # self.n = 100 # število partiklov
-        self.n = 150 # število partiklov
+
+        self.n = 100 # število partiklov
         self.sigma = 1 # koliko se razmečejo okoli ground truth (samo pri init)
-        self.sigma2 = 1 # kolko se kaznuje različnost od targeta (v obliki zmanjšanja uteži)
+        self.sigma2 = 4 # kolko se kaznuje različnost od targeta (v obliki zmanjšanja uteži)
+        # self.sigma2 = 2 # kolko se kaznuje različnost od targeta (v obliki zmanjšanja uteži)
         
-        self.A = np.array([[1,0,1,0],
-                  [0,1,0,1],
-                  [0,0,1,0],
-                  [0,0,0,1]])
-        self.Q = self.q * 1/6 * np.array([[2,0,3,0],
-                                          [0,2,0,3],
-                                          [3,0,6,0],
-                                          [0,3,0,6]])
+        # RW
+        
+        self.dynamic_mode = "NCV"
+        
         
         self.verbose = False
         
--- proj4/testing.py
+++ proj4/testing.py
@@ -6,6 +6,7 @@ from run_part_tracker_fun import run
 from part_tracker import PartParams
 
 def plot2(x, y1, y2, x_label, y1_label="n_failures", y2_label="avg_overlap"):
+    print("y2:", y2)
     fig, ax1 = plt.subplots()
 
     color = 'tab:red'
@@ -31,22 +32,30 @@ def run_tests(params=PartParams(),verbose=False):
     # tests = ["bicycle",  "car",  "david",   "face",        "hand",       "juice", "sunshade",  "woman",
     # "bolt",     "cup",  "diving",  "gymnastics", "iceskater",  "jump",   "singer",    "torus"]
     
-    tests = ["car",   "face",     "juice",  "woman",
+    tests = ["car",   "face",
        "cup",   "gymnastics",   "jump",      "torus"]
     
     n_fails = 0
+    n_fail = 0
     overlaps = 0
+    overlap = 0
+    FPSs = 0
+    FPS = 0
     for test in tests:
-        print(test,"...")
+        print(n_fail,overlap,FPS)
+        print(test,"...",end="")
         FPS, n_fail, overlap = run(sequence=test,parameters=PartParams(),plot=False,video_delay=15,verbose=verbose)
         n_fails += n_fail
-        overlaps += overlaps
-    return n_fails, overlaps/len(tests)
+        overlaps += overlap
+    print(n_fail,overlap,FPS)
+    return n_fails, overlaps/len(tests), FPSs/len(tests)
 
 def d2obj(d):
     parameters = PartParams()
     if "enlarge_factor" in d.keys():
         parameters.enlarge_factor = d["enlarge_factor"]
+    if "sigma_epanachnich" in d.keys():
+        parameters.sigma_epanachnich = d["sigma_epanachnich"]
     if "nbins" in d.keys():
         parameters.nbins = d["nbins"]
     if "hist_mode" in d.keys():
@@ -91,23 +100,26 @@ def d2obj(d):
 def neki(x_label, values):
     n_fails = []
     avg_overlaps = []
+    FPSs = []
     for val in values:
         parameters = d2obj({x_label:val})
         print(x_label,val)
-        n_fail, avg_overlap = run_tests(parameters)
+        n_fail, avg_overlap, FPS = run_tests(parameters)
         n_fails.append(n_fail)
         avg_overlaps.append(avg_overlap)
+        FPSs.append(FPS) # TODO
     plot2(values,n_fails, avg_overlaps, x_label)
     
 
 for x_label, values in [
-                        ("enlarge_factor",[2,2.5,3])
-                        # ("alpha",[0,0.01,0.1]),
-                        ("alpha",[0.1,0.15,0.2]),
+                        ("enlarge_factor",[1,2,2.5,3,3.5]),
+                        ("sigma_epanachnich",[1,2,3]),
+                        ("alpha",[0,0.01,0.1,0.2,0.3]),
                         ("q",[0.1,0.25,0.5,1,2]),
-                        ("r",[0.1,0.5,1,2]),
+                        ("r",[0.1,0.25,1,2]),
                         ("n",[50,100,150]),
-                        ("sigma2", [0.8,1,1.5])
+                        ("sigma2", [0.5,1,2,4,7]),
+                        ("dynamic_mode",["RW","NCV","NCA"])
                         ]:
     try:
         neki(x_label,values)
@@ -115,6 +127,8 @@ for x_label, values in [
     except Exception:
         print("exception at x_label, you need to retry")
 
+
+# neki("enlarge_factor",[2])
 
         
 # self.enlarge_factor = 2
@@ -130,4 +144,30 @@ for x_label, values in [
 # self.sigma = 1 # koliko se razmečejo okoli ground truth (samo pri init)
 # self.sigma2 = 1 # kolko se kaznuje različnost od targeta (v obliki zmanjšanja uteži)
 
+
+
+parameters = PartParams()
+# parameters.q = 1 / 37
+# parameters.r = 1
+# parameters.n = 100
+parameters.dynamic_mode = "NCV"
+# print(run_tests(parameters))
+# print(run("car",parameters,plot =True,verbose=False))
+
+
+
+def neki2(x_label, values):
+    n_fails = []
+    avg_overlaps = []
+    FPSs = []
+    for val in values:
+        parameters = d2obj({x_label:val})
+        print(x_label,val)
+        n_fail, avg_overlap, FPS = run_tests(parameters)
+        n_fails.append(n_fail)
+        avg_overlaps.append(avg_overlap)
+        FPSs.append(FPS) # TODO
+    plot2(values, n_fails, FPSs, x_label, y1_label="n_failures", y2_label="FPS")
+    plot2(values, avg_overlaps, FPSs, x_label, y1_label="avg_overlap", y2_label="FPS")
+neki2("n",[50,75,100,150])
         
